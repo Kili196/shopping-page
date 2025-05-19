cf00153591718c8e9f1bfd3fd849d4ab77e3d9d9
kili196 <kilian.baier16@gmail.com>
2025-05-19 11:21:26 +0200

    init

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  /** Nav bar --> Left Logo middle side links right side shopping card
+   * Hero Section --> HEro section with a cathc headline
+   * Product View Section --> products with a carousel --> with view more button
+   * Footer
+   *
+   */
+
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
