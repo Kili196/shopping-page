08e210637b96ae9e4f742978c190e3389308e860
kili196 <kilian.baier16@gmail.com>
2025-05-20 23:42:53 +0200

    add productoverview component
    add productoverview styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar/Navbar";
 import Layout from "./Layout/Layout";
 import Herobackground from "./Herosection/Herobackground";
 import Hero from "./Herosection/Hero";
+import Productoverview from "./Shopsections/Productoverview";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
           <Hero />
         </Layout>
       </Herobackground>
+      <Layout>
+        <Productoverview />
+      </Layout>
     </>
   );
 }
---
+++ src/components/Shopsections/Productoverview.jsx
@@ -0,0 +1,10 @@
+import styles from "./sections.module.css";
+
+export default function Productoverview() {
+  return (
+    <div className={styles["section--productoverview"]}>
+      <h1> Our newest Products</h1>
+      <div> Overview </div>
+    </div>
+  );
+}
