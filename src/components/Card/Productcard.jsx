38674549449a9e28fb49f39b2912b80db8902811
kili196 <kilian.baier16@gmail.com>
2025-06-01 21:42:08 +0200

    change card styling
    change html of card

--- src/components/Card/Productcard.jsx
+++ src/components/Card/Productcard.jsx
@@ -1,24 +1,24 @@
 import styles from "./Card.module.css";
-import ShopButton from "../Buttons/ShopButton";
 import AddCardButton from "../Buttons/AddCardButton";
 
 export default function Productcard({ setCart, product }) {
   return (
-    <>
-      <div className={styles["card"]}>
-        <div className={styles["card--container"]}>
-          <img src={product && product.image} className={styles} />
+    <div className={styles["card"]}>
+      <div className={styles["card--container"]}>
+        <img
+          src={product?.image}
+          alt={product?.title}
+          className={styles["card--image"]}
+        />
 
-          <div className={styles[""]}>
-            <section className={styles["card--headlines"]}>
-              <h3> {product && product.title}</h3>
-              <h4> {product && product.price}€</h4>
-              <p>{product && product.description}</p>
-            </section>
-          </div>
-        </div>
-        <AddCardButton onClick={setCart} product={product} />
+        <section className={styles["card--headlines"]}>
+          <h3>{product?.title}</h3>
+          <h4>{product?.price}€</h4>
+          <p>{product?.description}</p>
+        </section>
       </div>
-    </>
+
+      <AddCardButton onClick={setCart} product={product} />
+    </div>
   );
 }
