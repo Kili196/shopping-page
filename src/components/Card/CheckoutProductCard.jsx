dd3d75b9169199625ef0170743b3ebdccd246912
kili196 <kilian.baier16@gmail.com>
2025-06-03 21:08:47 +0200

    add html for CheckoutProductCard

--- src/components/Card/CheckoutProductCard.jsx
+++ src/components/Card/CheckoutProductCard.jsx
@@ -1,5 +1,18 @@
 import styles from "./CheckoutProductCard.module.css";
 
 export default function CheckoutProductCard() {
-  return <></>;
+  return (
+    <div className={styles["card--container"]}>
+      <div className={styles["card--content"]}>
+        <img></img>
+        <h1> Test</h1>
+        <label>
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisidolorem consloreme
+          laborum amet, aliquam quod vitae nulla!{" "}
+        </label>
+      </div>
+      <div className={styles["card--quantity"]}>2x</div>
+    </div>
+  );
 }
