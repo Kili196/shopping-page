5cf1c96559f2562d1cafb44d4e59fb1e3728bfde
kili196 <kilian.baier16@gmail.com>
2025-06-04 19:20:20 +0200

    add css for CheckoutProductCard.jsx

--- src/components/Card/CheckoutProductCard.jsx
+++ src/components/Card/CheckoutProductCard.jsx
@@ -4,13 +4,19 @@ export default function CheckoutProductCard() {
   return (
     <div className={styles["card--container"]}>
       <div className={styles["card--content"]}>
-        <img></img>
-        <h1> Test</h1>
-        <label>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisidolorem consloreme
-          laborum amet, aliquam quod vitae nulla!{" "}
-        </label>
+        <img src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"></img>
+        <div>
+          <h1> Mens Casual Premium Slim Fit T-Shirts</h1>
+          <label>
+            {" "}
+            Slim-fitting style, contrast raglan long sleeve, three-button henley
+            placket, light weight & soft fabric for breathable and comfortable
+            wearing. And Solid stitched shirts with round neck made for
+            durability and a great fit for casual fashion wear and diehard
+            baseball fans. The Henley style round neckline includes a
+            three-button placket.{" "}
+          </label>
+        </div>
       </div>
       <div className={styles["card--quantity"]}>2x</div>
     </div>
